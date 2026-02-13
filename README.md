f0b581eda5c7aa999f43453d35be028902480684
sarch28 <141343635+sarch28@users.noreply.github.com>
2026-02-12 19:30:40 -0600

    Agregar contenido al README y crear la página principal de Sara

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # test1_sara
 repositorio para mostrar mis skill trabajando con git
+
+
+## Institucion educativa
+Universidad tecnilogica de la selva
+
+## lugar de procedencia
+Ocosingo, Chiapas. 2026
---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sara's page</title>
+</head>
+<body>
+    <h1>
+        Bienvenido a la página de Sara, una estudiante de informática 
+        apasionada por la programación y el desarrollo web. 
+</body>
+</html>
+
